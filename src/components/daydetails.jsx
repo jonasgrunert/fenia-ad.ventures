f419e54eae503ed1c8f0de71718b4bd295ea0685
jonasgrunert <jonas@grunert-berlin.de>
2019-10-06 13:17:23 +0200

    Formatting for fullwidth images

--- src/components/daydetails.jsx
+++ src/components/daydetails.jsx
@@ -84,68 +84,71 @@ const DayDetails = ({ title, date, place, images, description, index, isSelected
     };
   }, [])
   return (
-    <article className="media">
-      <div className="media-content" style={{ overflow: "hidden" }}>
-        <h6 class="subtitle is-6">
-          <span className="icon">
-            <FontAwesomeIcon icon={faCalendarDay} />
+    <>
+      <article className="media">
+        <div className="media-content" style={{ overflow: "hidden" }}>
+          <h6 class="subtitle is-6">
+            <span className="icon">
+              <FontAwesomeIcon icon={faCalendarDay} />
+            </span>
+            {" " + new Date(date).toLocaleDateString()}
+            <span className="icon">
+              <FontAwesomeIcon icon={faMapPin} />
+            </span>
+            {" " + place}
+          </h6>
+          <h2 className="title is-4">{title}</h2>
+          <p className="content">{description}</p>
+
+        </div>
+        <div className="media-right">
+          <span
+            role="button"
+            tabIndex={0}
+            onClick={() => onSelect(index)}
+            className="icon is-large"
+          >
+            <FontAwesomeIcon icon={isSelected ? faMapMarkerAlt : faMapMarker} size="3x" />
           </span>
-          {" " + new Date(date).toLocaleDateString()}
-          <span className="icon">
-            <FontAwesomeIcon icon={faMapPin} />
-          </span>
-          {" " + place}
-        </h6>
-        <h2 className="title is-4">{title}</h2>
-        <p className="content">{description}</p>
-        <ul style={{ display: "flex", flexWrap: "nowrap", overflow: "auto" }}>
-          {images.map((image, idx) => (
-            <>
-              <li
+        </div>
+      </article>
+      <ul style={{ display: "flex", flexWrap: "nowrap", overflow: "auto" }}>
+        {images.map((image, idx) => (
+          <>
+            <li
+              role="button"
+              tabIndex={0}
+              onClick={() => dispatch(idx)}
+              className="image"
+              style={{ paddingRight: "10px" }}
+            >
+              <Img style={{ maxHeight: "100%", minWidth: "100%", objectFit: "cover", verticalAlign: "bottom" }} fixed={image.image.childImageSharp.fixed} alt={image.name} />
+            </li>
+            <div className={`modal ${currentModal === idx ? "is-active" : ""}`}>
+              <div
                 role="button"
                 tabIndex={0}
-                onClick={() => dispatch(idx)}
-                className="image"
-                style={{ paddingRight: "10px" }}
-              >
-                <Img style={{ maxHeight: "100%", minWidth: "100%", objectFit: "cover", verticalAlign: "bottom" }} fixed={image.image.childImageSharp.fixed} alt={image.name} />
-              </li>
-              <div className={`modal ${currentModal === idx ? "is-active" : ""}`}>
-                <div
-                  role="button"
-                  tabIndex={0}
-                  onClick={() => dispatch("close")}
-                  class="modal-background"
-                ></div>
-                <div class="modal-content">
-                  <div class="image">
-                    <Img
-                      fluid={image.image.childImageSharp.fluid}
-                      alt={image.name}
-                    />
-                  </div>
+                onClick={() => dispatch("close")}
+                class="modal-background"
+              ></div>
+              <div class="modal-content">
+                <div class="image">
+                  <Img
+                    fluid={image.image.childImageSharp.fluid}
+                    alt={image.name}
+                  />
                 </div>
-                <button
-                  onClick={() => dispatch("close")}
-                  class="modal-close is-large"
-                  aria-label="close"
-                ></button>
               </div>
-            </>
-          ))}
-        </ul>
-      </div>
-      <div className="media-right">
-        <span
-          role="button"
-          tabIndex={0}
-          onClick={() => onSelect(index)}
-          className="icon is-large"
-        >
-          <FontAwesomeIcon icon={isSelected ? faMapMarkerAlt : faMapMarker} size="3x" />
-        </span>
-      </div>
-    </article>
+              <button
+                onClick={() => dispatch("close")}
+                class="modal-close is-large"
+                aria-label="close"
+              ></button>
+            </div>
+          </>
+        ))}
+      </ul>
+    </>
   );
 };
 
